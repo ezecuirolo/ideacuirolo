59d3ab6f42766a8d0468ba8c25b941d28bc3f510
ezecuirolo <ezecuirolo@estudioindex.com.ar>
2021-08-04 22:25:15 -0300

    clase4

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,14 @@ import LogoQwines from './LogoQwines';
 import CartWidget from './CartWidget';
 
 const NavBar = props => {
-    //I have to loop all the items and convert them into LIs
     const items = props.items.map(item => (
-      <li className="nav-item">
+      <li key={item.id} className="nav-item">
         <a className="nav-link" href={item.url}>
           {item.label}
         </a>
       </li>
     ));
-  
-    //this methods says how the NavBar should look like in HTML
-    return (
+      return (
       <>
       <nav className="navbar">
         <ul>{items}</ul>
@@ -23,13 +20,12 @@ const NavBar = props => {
     );
   
   };
-  // declare a JS array with the menu items that you want
   let menu = [
-    { label: "Vinos", url: "/vinos" },
-    { label: "Olivas", url: "/olivas" },
-    { label: "Espumantes", url: "/espumantes" },
-    { label: "Aperitivos", url: "/aperitivos" },
-    { label: "Delicatessen", url: "/delicatessen" }
+    { id:"1",label: "Vinos", url: "/vinos" },
+    { id:"2",label: "Olivas", url: "/olivas" },
+    { id:"3",label: "Espumantes", url: "/espumantes" },
+    { id:"4",label: "Aperitivos", url: "/aperitivos" },
+    { id:"5",label: "Delicatessen", url: "/delicatessen" }
   ];
   const Componente = () => {
     return (<><LogoQwines/><NavBar items={menu}/></>);
